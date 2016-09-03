7fa2888daf835ec15c3fef718737bed74fd38a8e
Nathan Fisher <nfisher@junctionbox.ca>
2016-09-03 23:07:55 +0100

    Initialise app-db.

--- src/cljs/crr/core.cljs
+++ src/cljs/crr/core.cljs
@@ -1,5 +1,14 @@
 (ns crr.core
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            [re-frame.core :as rf]))
+
+(def initial-state
+  {:name "World 5"}) ; this is the map we want to initialise the app-db to.
+
+(rf/reg-event-db
+  :initialise
+  (fn [db _]
+    (merge db initial-state)))
 
 (defn hello-world [name] ; accept name as a parameter
   [:h1 "Hello " name]) ; instead of JSX, Reagent uses Hiccup which are simply nested vectors.
@@ -12,5 +21,6 @@
 
 (defn ^:export run
   []
+  (rf/dispatch-sync [:initialise])
   (reagent/render [hello-world-container ] ; hiccup that renders hello-world into page
                   (js/document.getElementById "app")))
