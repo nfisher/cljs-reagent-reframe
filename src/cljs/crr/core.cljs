ca410a1f27e3a978580eb6f5b5833c9ef9276d4c
Nathan Fisher <nfisher@junctionbox.ca>
2016-09-03 22:44:21 +0100

    Introduce the container.

--- src/cljs/crr/core.cljs
+++ src/cljs/crr/core.cljs
@@ -4,7 +4,13 @@
 (defn hello-world [name] ; accept name as a parameter
   [:h1 "Hello " name]) ; instead of JSX, Reagent uses Hiccup which are simply nested vectors.
 
+(defn hello-world-container []
+  ;; our container
+  (let [name "World 3"]
+    (fn []
+      [hello-world name])))
+
 (defn ^:export run
   []
-  (reagent/render [hello-world "World 2"] ; hiccup that renders hello-world into page
+  (reagent/render [hello-world-container ] ; hiccup that renders hello-world into page
                   (js/document.getElementById "app")))
