7fc65c971875a152f8b6965ba980029074d3eb4f
Nathan Fisher <nfisher@junctionbox.ca>
2016-09-03 23:55:29 +0100

    Huh strange that allowed me to println before w/out this.

--- env/dev/cljs/crr/dev.cljs
+++ env/dev/cljs/crr/dev.cljs
@@ -2,5 +2,7 @@
   (:require [crr.core :as crr]
             [figwheel.client :as fw]))
 
+(enable-console-print!)
+
 (fw/start {:on-jsload crr/run
            :websocket-url "ws://localhost:3449/figwheel-ws"})
