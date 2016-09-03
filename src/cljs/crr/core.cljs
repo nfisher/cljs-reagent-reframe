d0de654c830f67ec71ae2d8a2472ca5a3fffc59a
Nathan Fisher <nfisher@junctionbox.ca>
2016-09-03 22:42:41 +0100

    Create presentational component.

--- src/cljs/crr/core.cljs
+++ src/cljs/crr/core.cljs
@@ -1,10 +1,10 @@
 (ns crr.core
   (:require [reagent.core :as reagent]))
 
-(defn hello-world []
-  [:h1 "Hello World"]) ; instead of JSX, Reagent uses Hiccup which are simply nested vectors.
+(defn hello-world [name] ; accept name as a parameter
+  [:h1 "Hello " name]) ; instead of JSX, Reagent uses Hiccup which are simply nested vectors.
 
 (defn ^:export run
   []
-  (reagent/render [hello-world] ; hiccup that renders hello-world into page
+  (reagent/render [hello-world "World 2"] ; hiccup that renders hello-world into page
                   (js/document.getElementById "app")))
